650f64744b35cf152c1615235924cf27eccb4c53
Mathew <mathew_lemonde@hotmail.com>
2014-09-16 10:01:26 -0400

    Supression autre BD

--- HugoLand/HugoLand/Controleur/MainControleur.cs
+++ HugoLand/HugoLand/Controleur/MainControleur.cs
@@ -12,19 +12,29 @@ namespace HugoLand.Controleur
         rpggameEntities context = new rpggameEntities();
 
         #region MAP
-        public void CreateMap()
+        public void CreateMap(string sName,string sDescription,int iLevel,int iReqLevel) //TODO CHECKER PARAMS
         {
             Map map = new Map()
             {
-
+                Name = sName,
+                Description = sDescription,
+                Level = iLevel,
+                ReqLevel = iReqLevel
+           
             };
             context.Maps.Add(map);
             context.SaveChanges();
         }
-        public void EditMap(int id)
+        public void EditMap(int id,string sDesc) //TODO LIMITES
         {
             Map map = context.Maps.Find(id);
-
+            if (map == null)
+                return;
+            else
+            {
+                map.Description = sDesc;                
+            }
+            context.SaveChanges();
         }
         public void DeleteMap(Map map)
         {
