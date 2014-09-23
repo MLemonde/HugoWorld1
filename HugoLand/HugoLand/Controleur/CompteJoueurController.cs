11da4568dc9487e4e2ab3fc26b528835340d5d4b
Mathew <mathew_lemonde@hotmail.com>
2014-09-23 09:28:26 -0400

    Monstre et Compte

---
+++ HugoLand/HugoLand/Controleur/CompteJoueurController.cs
@@ -0,0 +1,58 @@
+﻿using HugoLand.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HugoLand.Controleur
+{
+    class CompteJoueurController
+    {
+        RpgGameEntities context = new RpgGameEntities();
+
+
+        public void CreatePlayer(string sUsername,string sPass,string sEmail,string sFname,string sLname,int iType)
+        {          
+            //TODO VRAI HASH
+            string sPassword = "soada11" + sPass + "neoi333`";
+            CompteJoueur Account = new CompteJoueur()
+            {
+                Courriel = sEmail,
+                NomUtilisateur = sUsername,
+                TypeUtilisateur = iType,
+                Nom = sLname,
+                Prenom = sFname,
+                Password = sPassword
+            };
+            context.CompteJoueurs.Add(Account);
+            context.SaveChanges();
+
+        }
+        public void DeletePlayer(int id)
+        {
+            CompteJoueur Account = context.CompteJoueurs.Find(id);
+            context.CompteJoueurs.Remove(Account);
+            context.SaveChanges();
+        }
+
+        public void EditPlayer(int id,string sEmail,string sFname,string sLname,int iType)
+        {
+            CompteJoueur Account = context.CompteJoueurs.Find(id);
+            if (Account == null)
+                return;
+
+            Account.Courriel = sEmail;
+            Account.Prenom = sFname;
+            Account.Nom = sLname;
+            Account.TypeUtilisateur = iType;
+
+            context.SaveChanges();
+        }
+
+        public void ValidatePlayer(string sUsername,string sPassword)
+        {
+            
+        }
+    }
+}
--- HugoLand/HugoLand/Controleur/MainControleur.cs
+++ HugoLand/HugoLand/Controleur/MainControleur.cs
@@ -9,45 +9,47 @@ namespace HugoLand.Controleur
 {
     class MainControleur
     {
-        rpggameEntities context = new rpggameEntities();
-
-        #region MAP
-        public void CreateMap(string sName,string sDescription,int iLevel,int iReqLevel) //TODO CHECKER PARAMS
-        {
-            Map map = new Map()
-            {
-                Name = sName,
-                Description = sDescription,
-                Level = iLevel,
-                ReqLevel = iReqLevel
-           
-            };
-            context.Maps.Add(map);
-            context.SaveChanges();
-        }
-        public void EditMap(int id,string sDesc) //TODO LIMITES
-        {
-            Map map = context.Maps.Find(id);
-            if (map == null)
-                return;
-            else
-            {
-                map.Description = sDesc;                
-            }
-            context.SaveChanges();
-        }
-        public void DeleteMap(Map map)
-        {
-            context.Maps.Remove(map);
-            context.SaveChanges();
-        }
-
-        public List<Map> GetListMap()
-        {
-            return context.Maps.ToList();
-        }
-        #endregion
-        #region 
-
+        RpgGameEntities context = new RpgGameEntities();
     }
 }
+
+//        #region MAP
+//        public void CreateMap(string sName,string sDescription,int iLevel,int iReqLevel) //TODO CHECKER PARAMS
+//        {
+//            Map map = new Map()
+//            {
+//                Name = sName,
+//                Description = sDescription,
+//                Level = iLevel,
+//                ReqLevel = iReqLevel
+           
+//            };
+//            context.Maps.Add(map);
+//            context.SaveChanges();
+//        }
+//        public void EditMap(int id,string sDesc) //TODO LIMITES
+//        {
+//            Map map = context.Maps.Find(id);
+//            if (map == null)
+//                return;
+//            else
+//            {
+//                map.Description = sDesc;                
+//            }
+//            context.SaveChanges();
+//        }
+//        public void DeleteMap(Map map)
+//        {
+//            context.Maps.Remove(map);
+//            context.SaveChanges();
+//        }
+
+//        public List<Map> GetListMap()
+//        {
+//            return context.Maps.ToList();
+//        }
+//        #endregion
+//        #region 
+
+//    }
+//}
