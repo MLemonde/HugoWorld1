b1c52557c831cb6a3acd3476020358e4b3b0c9d0
francis94 <l-frank@hotmail.com>
2014-09-23 09:07:47 -0400

    FirstOrNull et debut item

---
+++ HugoLand/HugoLand/Model/EntityHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HugoLand.Model
+{
+    public static class EntityHelper
+    {
+        public static T FirstOrNull<T>(this IEnumerable<T> list, Func<T, bool> predicate) where T : class
+        {
+            foreach (T item in list)
+                if (predicate(item))
+                    return item;
+            return null;
+        }
+    }
+}
