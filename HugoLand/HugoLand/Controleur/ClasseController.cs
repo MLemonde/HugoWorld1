28355610791b151a2ee8b9512f3e171ac0747019
Mathew <mathew_lemonde@hotmail.com>
2014-10-21 10:46:58 -0400

    ClasseController.cs modified

--- HugoLand/HugoLand/Controleur/ClasseController.cs
+++ HugoLand/HugoLand/Controleur/ClasseController.cs
@@ -10,19 +10,26 @@ namespace HugoLand.Controller
     class ClasseController
     {
         RpgGameEntities db = new RpgGameEntities();
-
+        /// <summary>
+        /// Auteur : Marc-André Landry
+        /// </summary>
+        /// <param name="sNomClasse"></param>
+        /// <param name="sDescription"></param>
+        /// <param name="fStatPoidsStr">Facteur bonus/malus Strength</param>
+        /// <param name="fStatPoidsDex">Facteur bonus/malus Dex</param>
+        /// <param name="fStatPoidsInt">Facteur bonus/malus Intel</param>
+        /// <param name="fStatPoidsStam">Facteur bonus/malus Stamina</param>
+        /// <param name="iMondeId">Monde dans lequel la classe est créer</param>
         public void CreateClass(string sNomClasse, string sDescription, float fStatPoidsStr, float fStatPoidsDex,
                                 float fStatPoidsInt, float fStatPoidsStam, int iMondeId)
         {
-            bool existanceDuMonde = false; // Vérifier que le monde qu'on où on créer la classe existe au minimum...
-            foreach (var item in db.Mondes)
-            {
-                if (item.Id == iMondeId)
-                    existanceDuMonde = true;
-            }
+            var Monde = db.Mondes.FirstOrNull(c => c.Id == iMondeId);
+            if (Monde == null)
+                return;
 
-            if (existanceDuMonde)
-            {
+            
+
+            
                 Classe myClass = new Classe()
                 {
                     NomClasse = sNomClasse,
@@ -34,59 +41,82 @@ namespace HugoLand.Controller
                     MondeId = iMondeId
                 };
                 db.Classes.Add(myClass);
+                Monde.Classes.Add(myClass);
                 db.SaveChanges();
-            }
+            
         }
 
+        /// <summary>
+        /// Auteur: Marc-André Landry
+        /// </summary>
+        /// <param name="iClassID"></param>
         public void DeleteClass(int iClassID)
         {
             Classe myClasse = db.Classes.Find(iClassID);
             if (myClasse == null)
                 return;
-            else
-                db.Classes.Remove(myClasse);
+
+            myClasse.Monde.Classes.Remove(myClasse);
+            db.Classes.Remove(myClasse);
             db.SaveChanges();
         }
 
+        /// <summary>
+        /// Auteur : Marc-André Landry
+        /// </summary>
+        /// <param name="iClasseID">Id de la classe</param>
+        /// <param name="sNomClasse">Nom de la classe</param>
+        /// <param name="sDescription">Description de la classe</param>
+        /// <param name="fStatPoidsStr">Facteur bonus/malus Strength</param>
+        /// <param name="fStatPoidsDex">Facteur bonus/malus Dex</param>
+        /// <param name="fStatPoidsInt">Facteur bonus/malus Intel</param>
+        /// <param name="fStatPoidsStam">Facteur bonus/malus Stamina</param>
+        /// <param name="iMondeID">Id du monde</param>
         public void EditClassFromWorld(int iClasseID, string sNomClasse, string sDescription, float fStatPoidsStr, 
                                         float fStatPoidsDex, float fStatPoidsInt, float fStatPoidsStam, int iMondeID)
         {
             Classe myClasse = db.Classes.Find(iClasseID);
             if (myClasse == null)
                 return;
-            else
-            {
-                foreach (var item in db.Classes)
-                {
-                    if(item.MondeId == iMondeID)
-                    {
-                        myClasse.Description = sNomClasse;
-                        myClasse.StatPoidsStr = fStatPoidsStr;
-                        myClasse.StatPoidsDex = fStatPoidsDex;
-                        myClasse.StatPoidsInt = fStatPoidsInt;
-                        myClasse.StatPoidsStam = fStatPoidsStam;
-                    }
-                }
-            }
+
+            if (myClasse.MondeId != iMondeID)
+                return;
+
+                
+             myClasse.Description = sNomClasse;
+             myClasse.StatPoidsStr = fStatPoidsStr;
+             myClasse.StatPoidsDex = fStatPoidsDex;
+             myClasse.StatPoidsInt = fStatPoidsInt;
+             myClasse.StatPoidsStam = fStatPoidsStam;
+                    
+                
+            
             db.SaveChanges();
         }
 
-        public List<Classe> GetListMap(int mondeID)
+        /// <summary>
+        /// Auteur: Mathew Lemonde
+        /// Retourne liste de classe
+        /// </summary>
+        /// <param name="mondeID"></param>
+        /// <returns></returns>
+        public List<Classe> GetListClasses(int mondeID)
         {
-            List<Classe> listOfClasses = new List<Classe>();
+            var Monde = db.Mondes.FirstOrNull(c => c.Id == mondeID);
+            if (Monde == null)
+                return null;
 
-            foreach (var item in db.Classes)
-            {
-                if (item.MondeId == mondeID)
-                    listOfClasses.Add(item);
-            }
-            return listOfClasses;
+            return Monde.Classes.ToList();            
         }
-
-        // Recevoir toutes les classes pour un monde donné (clé)
         
-
-        public string FindClasseOfHero(int iHeroID, int iMondeID)
+        /// <summary>
+        /// Auteur : Marc-André Landry
+        /// Trouver la classe d'un héro
+        /// </summary>
+        /// <param name="iHeroID">Id du héro</param>
+        /// <param name="iMondeID">Id du Monde</param>
+        /// <returns></returns>
+        public Classe FindClasseOfHero(int iHeroID, int iMondeID)
         {
             bool WorldExistance = false;
             bool HeroExistance = false;
@@ -109,11 +139,11 @@ namespace HugoLand.Controller
                            p.Id == iHeroID
                            select p.Classe;
 
-                return ClassOfHero.ToString();
+                return ClassOfHero.First();
             }
             else
             {
-                return "Le héro ou le monde demander n'existe pas. GG";
+                return null;
             }
         }
     }
